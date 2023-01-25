3cd1c7b7fad5c8da9068bda97a4e516f45e865b1
0763866972 <123261086+0763866972@users.noreply.github.com>
2023-01-25 21:54:41 +0700

    Update Chapter1.cpp

--- Chapter1.cpp
+++ Chapter1.cpp
@@ -1,5 +1,5 @@
 
-// CHÚ Ý CỰC KỲ: là các câu lệnh sẽ thực hiện theo thứ tự mà các bạn code từ trái qua phải và từ trên xuống dưới 
+// CHÚ CỰC KỲ: là các câu lệnh sẽ thực hiện theo thứ tự mà các bạn code từ trái qua phải và từ trên xuống dưới 
 
                                    
                                     // để cách khoảng khi in ra màn thì ta cách khoảng trong dấu " " này 
