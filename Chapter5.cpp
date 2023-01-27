ed3c8e754acc1531b0375b832ca6de50544317a4
phan tan phat <phantanphat@MacBook-Pro-cua-phan.local>
2023-01-27 20:46:08 +0700

    l

--- Chapter5.cpp
+++ Chapter5.cpp
@@ -1,2 +1,2 @@
 //                                                              vòng lặp while và do while
-// cấu trúc vòng lặp while (điều kiện){ code khi điều kiện   } thấy thì giống vs if nhưng nó khác á nha 
+// cấu trúc vòng lặp while (điều kiện){ code và điều kiện lặp của lệnh while  } thấy thì giống vs if nhưng nó khác á nha 
