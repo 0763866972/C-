59d0455a303ce8f8ba6de3d3ba273b2b4ca94589
phan tan phat <phantanphat@MacBook-Pro-cua-phan.local>
2023-01-27 09:57:22 +0700

    l

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -1,4 +1,4 @@
-                                                           // DẠNG câu lệnh IF else if eles loo alo 
+                                                           // DẠNG câu lệnh IF else if eles
 // && lệnh and nó đúng khi tất cả các điều kiện  đều đúng
 // || lệnh or  nó đúng khi một trong nhiều điều kiện nó đúng
 
